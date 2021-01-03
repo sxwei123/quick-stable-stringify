d748301c79d0bfb8c2b63d442bf0eca1fad64681
Leon Song <sxwei123@users.noreply.github.com>
2021-01-03 10:12:35 +0800

    Improve comparator function typing

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,20 @@
-declare module 'fast-json-stable-stringify' {
-  function stringify(obj: any): string;
+declare module "fast-json-stable-stringify" {
+  interface KeyValue {
+    key: string;
+    value: any;
+  }
+
+  type ComparatorFunction = (a: KeyValue, b: KeyValue) => number;
+
+  function stringify(
+    obj: any,
+    opts?:
+      | {
+          cmp?: ComparatorFunction;
+          cycles?: boolean;
+        }
+      | ComparatorFunction
+  ): string;
+
   export = stringify;
 }
